f16d77ffb79a7400f5c87a0a7a49e484e86b9a21
Md Arif Hosain <arifhosain20284@gmail.com>
2024-03-06 17:07:43 +0600

    ok

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 <!-- End Top bar -->
 
 <!-- Start Header -->
-<section class="bg-light">
+<section class="p-2">
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-between">
       <div class="container-fluid">
@@ -50,22 +50,22 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
-        <li class="nav-item">
+        <li class="nav-item fw-bold">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
 
-        <li class="nav-item">
+        <li class="nav-item fw-bold">
           <a class="nav-link active" aria-current="page" href="#">About</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item fw-bold">
           <a class="nav-link active" aria-current="page" href="#">Blog</a>
         </li>
         
-        <li class="nav-item">
+        <li class="nav-item fw-bold">
           <a class="nav-link active" aria-current="page" href="#">Services</a>
         </li>
 
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown fw-bold">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Pages
           </a>
@@ -76,7 +76,7 @@
           </ul>
         </li>
 
-        <li class="nav-item">
+        <li class="nav-item fw-bold">
           <a class="nav-link active" aria-current="page" href="#">Contact</a>
         </li>
 
@@ -95,37 +95,37 @@
   <div class="">
   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active bg-primary" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" class="bg-primary" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" class="bg-primary" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
+  <div class="carousel-inner ">
+    <div class="carousel-item active slider-bg">
       <img src="img/slider1.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h1>Meet with us to</h1>
-        <h1>success dream <span class="text-danger">business</span></h1>
-        <p class="pt-3 pb-3">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
+        <h1 class="text-light">Meet with us to</h1>
+        <h1 class="text-light">success dream <span class="text-primary">business</span></h1>
+        <p class="pt-3 pb-3 text-light">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
           in some form by injected humour randomised</p>
           <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item slider-bg">
       <img src="img/slider2.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h1>Meet with us to</h1>
-        <h1>success dream <span class="text-danger">business</span></h1>
-        <p class="pt-3 pb-3">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
+        <h1 class="text-light">Meet with us to</h1>
+        <h1 class="text-light">success dream <span class="text-primary">business</span></h1>
+        <p class="pt-3 pb-3 text-light">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
           in some form by injected humour randomised</p>
           <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item slider-bg">
       <img src="img/slider3.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h1>Meet with us to</h1>
-        <h1>success dream <span class="text-danger">business</span></h1>
-        <p class="pt-3 pb-3">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
+        <h1 class="text-light">Meet with us to</h1>
+        <h1 class="text-light">success dream <span class="text-primary">business</span></h1>
+        <p class="pt-3 pb-3 text-light">There are many variations of passages of Lorem Ipsum available but the majority have sufered alteration<br>
           in some form by injected humour randomised</p>
           <button class="btn btn-primary">Get Started</button>
       </div>
@@ -148,9 +148,9 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-sm-12">
-         <p class="pt-80 h2">About Us</p>
-  <div class="bb"></div>
-         <p class="pt-3">Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, 
+         <h2 class="pt-80 text-dark fw-bold">About Us</h1>
+           <div class="bb"></div>
+         <p class="pt-5 text-secondary">Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, 
           nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit.<br><br>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
          parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
@@ -167,51 +167,58 @@
   </div>
 </section>
 
-<section class="bg-light">
+<section class="area-bg">
   <div class="container">
-    <div class="row pb-80 pt-80">
+    <div class="row pb-80">
       <div class="col-12 text-center">
-      <h2 class="">Our Services <span class="bb"></span></h2>
-      <div class="text-center">
-      <p class=" bb"></p>
-      </div>
-      <p class="py-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
+      <h2 class="sec-title">Our Services </h2>
+      <div><span class="bb"></span></div>
+      <p class="py-5 text-secondary">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
        some form,words which don't look even slightly believable.</p>
       </div>
 
     <div class="row ">
       <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
        <div class="border rounded-3">
-        <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <img src="img/layer-group-solid.svg" class="w-25 pt-5" alt="">
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
         <button class="btn1 btn-secondery py-4 bg-light" >Read More</button>
         </div>
       </div>
 
       <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
        <div class="border rounded-3">
-        <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <img src="img/algolia-brands.svg" class="w-25 pt-5" alt="">
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
         <button class="btn1 btn-secondery py-4 bg-light" >Read More</button>
         </div>
       </div>
 
       <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
        <div class="border rounded-3">
-        <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <img src="img/air-freshener-solid.svg" class="w-25 pt-5" alt="">
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
         <button class="btn1 btn-secondery pt-4 py-4 bg-light" >Read More</button>
         </div>
       </div>
 
       <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
        <div class="border rounded-3">
-        <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <img src="img/ambulance-solid.svg" class="w-25 pt-5" alt="">
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <button class="btn1 btn-secondery py-4 bg-light" >Read More</button>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
+       <div class="border rounded-3">
+        <img src="img/american-sign-language-interpreting-solid.svg" class="w-25 pt-5" alt="">
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
         <button class="btn1 btn-secondery py-4 bg-light" >Read More</button>
         </div>
       </div>
@@ -219,68 +226,56 @@
       <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
        <div class="border rounded-3">
         <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
-        <button class="btn1 btn-secondery py-4 bg-light" >Read More</button>
-        </div>
-      </div>
-
-      <div class="col-md-4 col-sm-6 col-12 text-center pt-80 p-3">
-       <div class="border rounded-3">
-        <img src="img/address-card-solid.svg" class="w-25 pt-5" alt="">
-        <h5 class="py-2">Designs & interfaces</h5>
-        <p class="p-4">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
+        <h5 class="py-2 sec-title2">Designs & interfaces</h5>
+        <p class="p-4 text-secondary">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.</p>
         <button class="btn1 btn-secondery pt-4 py-4 bg-light" >Read More</button>
         </div>
       </div>
-
     </div>
-
     </div>
   </div>
 </section>
 
-<!-- Counter -->
-<section>
+<!--Start Counter -->
+<section class="counter">
   <div class="container">
     <div class="row text-center pt-80 pb-80">
       <div class="col-md-3 col-sm-6 col-12">
-      <i class="bi bi-people-fill fs-5 text-primary"></i>
-      <h2>5780</h2>
-      <h4>Happy Customers</h4>
+      <i class="bi bi-people-fill fs-5 text-primary fs-1"></i>
+      <h2 class="text-light">5780</h2>
+      <h4 class="text-light">Happy Customers</h4>
       </div>
 
       <div class="col-md-3 col-sm-6 col-12">
-      <i class="bi bi-hand-thumbs-up fs-5 text-primary"></i>
-      <h2>4920</h2>
-      <h4>Project Complete</h4>
+      <i class="bi bi-hand-thumbs-up fs-5 text-primary fs-1"></i>
+      <h2 class="text-light">4920</h2>
+      <h4 class="text-light">Project Complete</h4>
       </div>
 
       <div class="col-md-3 col-sm-6 col-12">
-      <i class="bi bi-buildings fs-5 text-primary"></i>
-      <h2>1760 </h2>
-      <h4>World Wide Branch</h4>
+      <i class="bi bi-buildings fs-5 text-primary fs-1"></i>
+      <h2 class="text-light">1760 </h2>
+      <h4 class="text-light">World Wide Branch</h4>
       </div>
 
       <div class="col-md-3 col-sm-6 col-12">
-      <i class="bi bi-printer fs-5 text-primary"></i>
-      <h2>2738</h2>
-      <h4>Digital Instrument</h4>
+      <i class="bi bi-printer fs-5 text-primary fs-1"></i>
+      <h2 class="text-light">2738</h2>
+      <h4 class="text-light">Digital Instrument</h4>
       </div>
     </div>
   </div>
 </section>
+<!-- End Counter -->
 
-<!-- Project -->
+<!--Start Project -->
 <section class="bg-light">
   <div class="container">
-    <div class="row pb-80 pt-80">
+    <div class="row pb-80">
       <div class="col-12 text-center">
-      <h2 class="">Our Services <span class="bb"></span></h2>
-      <div class="text-center">
-      <p class=" bb"></p>
-      </div>
-      <p class="py-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
+      <h2 class="sec-title">Our Services</h2>
+      <div><span class="bb"></span></div>
+      <p class="py-5 text-secondary">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
        some form,words which don't look even slightly believable.</p>
       </div>
 
@@ -362,6 +357,7 @@
     </div>
   </div>
 </section>
+<!-- End Project -->
 
 <!-- Slider Testmonial -->
 <section>
@@ -395,17 +391,18 @@
  </div>
 
 </section>
+<!-- End SLider Testmonial -->
 
-<!-- Team -->
-<section class="bg-light">
+<!--Start Our Team -->
+<section class="area-bg">
   <div class="container">
-    <div class="row pb-80 pt-80">
+    <div class="row pb-80">
       <div class="col-12 text-center">
-      <h2 class="">Our Team <span class="bb"></span></h2>
-      <div class="text-center">
-      <p class=" bb"></p>
+      <h2 class="sec-title">Our Team </h2>
+      <div class="">
+      <span class="bb"></span>
       </div>
-      <p class="py-5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
+      <p class="py-5 text-secondary">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
        some form,words which don't look even slightly believable.</p>
       </div>
 
@@ -465,6 +462,71 @@
   </div>
 </section>
 
+<!-- Watch Vedio -->
+<section class="vedio" style="background-image: url('img/cta1.jpg');">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-12 text-center text-light pb-80">
+       <h2 class="pt-80 pb-5">Developing first class solutions<br>for our clients.</h2>
+       <a href="https://www.youtube.com/watch?v=uxZR17y9xzQ&ab_channel=JamunaTV" target="_blank"><button class="btn btn-primary rounded-pill">WATCH VIDEO</button></a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End Vedio Section -->
+
+<!--Start Latest Blog -->
+<section class="area-bg">
+  <div class="container">
+    <div class="row pb-80">
+      <div class="col-12 text-center">
+      <h2 class="sec-title">Latest Blog </h2>
+      <div class="">
+      <span class="bb"></span>
+      </div>
+      <p class="py-5 text-secondary">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in<br>
+       some form,words which don't look even slightly believable.</p>
+      </div>
+
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+
+      <div class="col shadow-lg">
+    <div class="card h-100 border-0">
+      <img src="img/03 (1).jpg" class="card-img-top p-3" alt="...">
+      <div class="card-body">
+        <p class="card-text">08 APRIL 2019</p>
+        <h5 class="card-title text-dark pt-3 pb-3">Welcome to Jekyll! Go ahead and edit</h5>
+        <a href="blog.php"><button class="btn1 btn-secondery bg-white" >Read More</button></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col shadow-lg">
+    <div class="card h-100 border-0">
+      <img src="img/09.jpg" class="card-img-top p-3" alt="...">
+      <div class="card-body">
+        <p class="card-text">10 MARCH 2019</p>
+        <h5 class="card-title text-dark pt-3 pb-3">WordPress hostin manage your growth Nullam non nisi est</h5>
+        <a href="blog.php"><button class="btn1 btn-secondery bg-white" >Read More</button></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col shadow-lg">
+    <div class="card h-100 border-0">
+      <img src="img/05.jpg" class="card-img-top p-3" alt="...">
+      <div class="card-body">
+        <p class="card-text">09 MARCH 2019</p>
+        <h5 class="card-title text-dark pt-3 pb-3">html5 Static Site Generator For All amet facilisis</h5>
+        <a href="blog.php"><button class="btn1 btn-secondery bg-white" >Read More</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  </div>
+</section>
 
 <!-- Bootstarp Jave script link -->
 <script src="js/bootstrap.bundle.min.js"></script>
